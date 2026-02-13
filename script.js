ac68ed69639b618b0a2ff7b3ab9093ab77b65b51
takoyaki-lover <ss2004722@hotmail.com>
2026-02-13 23:45:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ const container = document.querySelector(".container");
 
 let isDragging = false;
 
-dragbar.addEventListener("pointerdown", function (e) {
+let isSupportTouch = "ontouchend" in document;
+console.log(isSupportTouch);
+
+dragbar.addEventListener(isSupportTouch ? "touchstart" : "mousedown", function (e) {
   e.preventDefault();
   isDragging = true;
   document.body.style.cursor = "ew-resize";
   document.body.style.userSelect = "none";
 });
 
-document.addEventListener("pointermove", function (e) {
+document.addEventListener(isSupportTouch ? "touchmove" : "mousemove", function (e) {
   if (!isDragging) return;
 
   // 現在のマウスの位置からコンテナの左端までの距離を取得
@@ -22,7 +25,7 @@ document.addEventListener("pointermove", function (e) {
   sidebar.style.width = newWidth + "px";
 });
 
-document.addEventListener("pointerup", function (e) {
+document.addEventListener(isSupportTouch ? "touchend" : "mouseup", function (e) {
   if (isDragging) {
     isDragging = false;
     document.body.style.cursor = "default";
